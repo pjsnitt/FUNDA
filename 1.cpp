781f3d5c94f6746e3d09c35b46289ec9bd9496b8
pjsnitt <pjsnitt@gmail.com>
2017-04-21 16:25:24 +0530

    1.cpp

---
+++ 1.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+#include<math.h>
+#include<stdlib.h>
+
+
+void main()
+{
+int a;
+cin>>a;
+if(a%2==0&&a!=2)
+{
+cout<<"Not Prime";
+exit(0);
+}
+else if(a==2!!a==1)
+{
+cout<<"Prime";
+exit(0);}
+else()
+{
+for(int i=3;i<sqrt(a);i=+2)
+{if(a%i==0)
+{cout<<"Not Prime";exit(0);
+}
+ cout<<"Prime";
+}
+}
